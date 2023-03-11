70f41705f888228e707d604f5c4daf297cbbabea
DanielAntic05 <124038764+DanielAntic05@users.noreply.github.com>
2023-03-11 11:55:38 +0100

    Add files via upload

--- ordRicorsivo/numeroInLettere.js
+++ ordRicorsivo/numeroInLettere.js
@@ -1,6 +1,7 @@
-let n = Number(prompt("Inserisci un numero" ));
-let a = [1,2,3,4,5,6,7,8,9];
-let strings = {
+let n = prompt("Inserisci un numero" );
+let a = [0,1,2,3,4,5,6,7,8,9];
+let strings = [{
+    0: "zero",
     1: "uno",
     2: "due",
     3: "tre",
@@ -10,6 +11,8 @@ let strings = {
     7: "sette",
     8: "otto",
     9: "nove",
+},
+{
     10: "dieci",
     11: "undici",
     12: "dodici",
@@ -17,7 +20,7 @@ let strings = {
     14: "quattordici",
     15: "quindici",
     16: "sedici",
-    17: "diciasette",
+    17: "diciassette",
     18: "diciotto",
     19: "dicianove",
     20: "venti",
@@ -28,13 +31,38 @@ let strings = {
     70: "settanta",
     80: "ottanta",
     90: "novanta",
+},
+{
     100: "cento",
+},
+{
     1000: "mille",
+},
+{
     10000: "mila",
+},
+{
     1000000: "milione"
-};
-tostring(n,a,strings);
+}];
+let nStringa = "";
 
-function tostring(n,a,strings){
+tostring(n,strings,nStringa);
 
+function tostring(n,strings,nStringa){
+let counter = 0;
+let l = 0;
+let counter2 = 0;
+counter2 =  counterNumbers(n,counter,l);
+if ( counter2 != 0){
+let key = Number(counterNumbers(n,counter,l));
+nStringa = nStringa + strings[counter2-1][key];
+toString(n,strings,nStringa);
+}
+}
+function counterNumbers(n,counter,l){
+    if (counter == n.length)
+        return counter;
+    else
+        counter++;
+        counterNumbers(n,counter,l+1);
 }
