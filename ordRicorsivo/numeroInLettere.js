77b96c79bb0d90f77bdedd09913df008c9dbd087
Daniel Antic <windowsc045@gmail.com>
2023-03-11 10:08:25 +0100

    es number to string

---
+++ ordRicorsivo/numeroInLettere.js
@@ -0,0 +1,40 @@
+let n = Number(prompt("Inserisci un numero" ));
+let a = [1,2,3,4,5,6,7,8,9];
+let strings = {
+    1: "uno",
+    2: "due",
+    3: "tre",
+    4: "quattro",
+    5: "cinque",
+    6: "sei",
+    7: "sette",
+    8: "otto",
+    9: "nove",
+    10: "dieci",
+    11: "undici",
+    12: "dodici",
+    13: "tredici",
+    14: "quattordici",
+    15: "quindici",
+    16: "sedici",
+    17: "diciasette",
+    18: "diciotto",
+    19: "dicianove",
+    20: "venti",
+    30: "trenta",
+    40: "quaranta",
+    50: "cinquanta",
+    60: "sessanta",
+    70: "settanta",
+    80: "ottanta",
+    90: "novanta",
+    100: "cento",
+    1000: "mille",
+    10000: "mila",
+    1000000: "milione"
+};
+tostring(n,a,strings);
+
+function tostring(n,a,strings){
+
+}
