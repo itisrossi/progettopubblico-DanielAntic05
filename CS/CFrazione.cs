46fde93209a89f67bd48e7a47b37e38d43ba9ca1
Daniel Antic <windowsc045@gmail.com>
2023-04-27 22:27:02 +0200

    Fisso, modifiche calcolatrice

---
+++ CS/CFrazione.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Frazioni
+{
+    class CFrazione
+    {
+        private int _num, _den; // usabili solo in questa classe
+
+        // num è una variabile pubblica, quindi si può usare nei costruttori
+        public int num // usabili anche da fuori = property dell'oggetto
+        {
+            get
+            {
+                return _num;
+            }
+            set
+            {
+                _num = value;
+            }
+        }
+        // servono per controllare se i valori 
+        // dati in input sono accettabili
+        public int den
+        {
+            get
+            {
+                return _den;
+            }
+            set
+            {
+                if (value != 0)
+                    _den = value;
+
+                else
+                    _den = 1; 
+            }
+        }
+        public int cambia = 1;
+        public CFrazione() //costruttore di default
+        {
+            num = 0; //è come mettere this.num = 0; 
+            den = 0; //con la property me lo mette lui a 1
+        }
+// _____________________________________________________________________________________________________________
+// da qui iniziano i costruttori ai quali accedono f1, f2, f3, f4, f5 nel file "Program.cs"
+        public CFrazione(int num, int den) //overloading costruttore = diamo più informazioni
+        {
+            // num property è uguale a num parametro
+            this.num = num; 
+            this.den = den;
+        }
+// __________________________________________________________________________________________________________________________
+// Da qui si calcola il risultato --> r
+
+    // Somma
+        // static = di classe
+        public static CFrazione Somma(CFrazione f1, CFrazione f2)
+        {
+            CFrazione ris;
+            ris = new CFrazione();
+            ris.den = f1.den * f2.den;
+            ris.num = (ris.den / f1.den) * f1.num + (ris.den / f2.den) * f2.num;
+            return ris.semplifica(ris);
+        }
+        // Altro modo per fare la somma:
+
+        // public CFrazione Somma(CFrazione f1) // da fare sull'oggetto
+        // {
+        //     CFrazione ris = CFrazione.Somma(this, f1); //per non ricopiare il codice 
+        //                                                // this = l'oggetto su cui applico la funzione
+        //     return ris;
+        // }
+
+        //override dell'operatore +
+        public static CFrazione operator +(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris = CFrazione.Somma(f1, f2);
+            return ris;
+        }
+
+         // override sovrascrive il ToString:
+
+         /* permette di estendere o modificare l'implementazione 
+            astratta o virtuale di un metodo, una proprietà,
+            un indicizzatore o un evento ereditato.
+
+            Infatti, siccome esiste gia' il metodo ToString(),
+            per farne uno nostro usiamo l'override.
+        */
+        // Forse (Si esegue da solo):
+        public override string ToString() // da warning perchè è già presente in ogni classe il metodo ToString
+        { // override per sovrascrivere e togliere il warning :)
+            string risultato = "";
+            risultato = this.num.ToString() + "/" + this.den.ToString();
+            return risultato;
+        }
+
+        // // non c'e' l'override perche' c'e' un bool
+        // public string ToString(bool json)
+        // {
+        //         string risultato = "";
+        //         if (json)
+        //                         // JsonSerializer classe che permette di trasformare una stringa json
+        //             risultato = JsonSerializer.Serialize(this);
+        //         else
+        //             risultato = risultato.ToString();
+
+        //         return risultato;
+        // }
+
+        //Sottrazione
+        public static CFrazione Sottrazione(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris;
+            ris = new CFrazione();
+            ris.den = f1.den * f2.den;
+            ris.num = (ris.den / f1.den) * f1.num - (ris.den / f2.den) * f2.num;
+            return ris.semplifica(ris);
+        }
+
+        // public CFrazione Sottrazione(CFrazione f1) // da fare sull'oggetto
+        // {
+        //     CFrazione ris = CFrazione.Sottrazione(this, f1);
+        //     return ris.semplifica(ris);
+        // }
+
+        //override dell'operatore -
+        public static CFrazione operator -(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris = CFrazione.Sottrazione(f1, f2);
+            return ris;
+        }
+
+        //Moltiplicazione
+        public static CFrazione Moltiplicazione(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris;
+            ris = new CFrazione();
+            ris.den = f1.den * f2.den;
+            ris.num = f1.num * f2.num;
+            return ris.semplifica(ris);
+        }
+
+        // public CFrazione Moltiplicazione(CFrazione f1) // da fare sull'oggetto
+        // {
+        //     CFrazione ris = CFrazione.Moltiplicazione(this, f1);
+        //     return ris;
+        // }
+
+        //override dell'operatore *
+        public static CFrazione operator *(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris = CFrazione.Moltiplicazione(f1, f2);
+            return ris;
+        }
+
+        // public CFrazione reciproco()
+        // {
+        //     CFrazione risultato = new CFrazione (this.den, this.num);
+        //     return risultato;
+        // }
+        //Divisione
+        public static CFrazione Divisione(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris;
+            ris = new CFrazione();
+            ris.den = f1.den * f2.num;
+            ris.num = f1.num * f2.den;
+            return ris.semplifica(ris);
+        }
+
+        // public CFrazione Divisione(CFrazione f1) // da fare sull'oggetto
+        // {
+        //     CFrazione ris = CFrazione.Divisione(this, f1);
+        //     return ris;
+        // }
+
+        //override dell'operatore /
+        public static CFrazione operator /(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris = CFrazione.Divisione(f1, f2);
+            return ris;
+        }
+        public CFrazione semplifica(CFrazione result)
+        {
+            int mcd = 0;
+            
+            // CFrazione.MCD si può usare se MCD è static ----> private static int MCD() !!!!!
+            mcd = MCD(result.num, result.den); 
+                result.num /= mcd;
+                result.den /= mcd;
+            // return dell'oggetto cambiato
+                return result;
+                
+            }   
+        private int MCD(int n1, int n2)
+        {
+            // Math.Abs() restituisce il valore assoluto di un numero!
+            n1 = Math.Abs(n1);
+
+            n2 = Math.Abs(n2);
+
+            while (n1 != 0 && n2 != 0){
+                if (n1 > n2)
+                    n1 %= n2;
+                else
+                    n2 %= n1;
+            }
+
+            /* oppure
+            while (n1 != n2)
+            {
+                if (n1 > n2) n1 = n1 - n2;
+                else n2 = n2 - n1;
+            }
+            return a;
+            */
+
+            // | serve per far ritornare il valore di n1 e n2 piu' grande!
+            Console.WriteLine("OR {0}", n1 | n2);
+            return n1 | n2;
+        }
+    }
+}
---
+++ CS/HugeN.cs
@@ -0,0 +1,86 @@
+﻿/* 
+Scrivere un programma che riesce a stampare un 
+numero molto grande (esempio: con 300 cifre).
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Frazioni
+{
+    class CHugeNumber
+    {
+        private const int N = 300;
+        private int[] mCifre;
+
+        public int[] Cifre
+        {
+            get { return mCifre; }
+            set { mCifre = value; }
+        }
+
+        public CHugeNumber()
+        {
+            // metti un puntatore a un oggetto che è un vettore di tipo int
+            Cifre = new int[N]; // cosi' si crea anche un vettore con 0 in tutte le celle
+        }
+        // usiamo una stringa perche' puo' possedere piu' caratteri di un int, double, ecc.
+        public CHugeNumber(string numero)
+        {
+            Cifre = new int[N];
+            if (numero.Length < N) {
+                for (int i = 0; i < numero.Length; i++)
+                {
+                    // Cifre[N - i - 1] = numero[numero.Length - i - 1] - "0";
+                    Cifre[N - i - 1] = Convert.ToInt32(numero[i]) - 48;
+                }
+            }   else {
+                    throw new InvalidOperationException();
+                }
+        }
+        
+        // Somma
+        public static CHugeNumber operator +(CHugeNumber n1, CHugeNumber n2){
+            // si crea l'array con lunghezza N tramite il costruttore public CHugeNumber()
+            CHugeNumber risultato = new CHugeNumber();
+
+            int Riporto = 0;
+
+            for (int i = N - 1; i >= 0; i--)
+            {
+                risultato.Cifre[i] = n1.Cifre[i] + n2.Cifre[i] + Riporto;
+                    if (risultato.Cifre[i] > 9)
+                    {
+                        Riporto = 1;
+                        risultato.Cifre[i] -= 10;
+                        // Controllo overflow
+                        if (i == 0)
+                            throw new OverflowException();
+                    } else
+                        Riporto = 0;
+                    }
+            return risultato;
+        }
+
+        // Sottrazione
+        public static CHugeNumber operator -(CHugeNumber n1, CHugeNumber n2){
+            CHugeNumber risultato = new CHugeNumber();
+
+            risultato = n1 + n2.complemento();
+            return risultato;
+        }
+        private CHugeNumber complemento()
+        {
+            CHugeNumber risultato = new CHugeNumber();
+            CHugeNumber uno = new CHugeNumber("+1");
+
+            for (int i = N - 1; i >= 0; i--)
+            {
+                risultato.Cifre[i] = 9 - this.Cifre[i];
+            }
+            return risultato + uno;
+        }
+
+    }
+}
